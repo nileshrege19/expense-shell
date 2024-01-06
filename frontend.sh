a7c27be8eb03752515a429de844b5d244f911c00
nilesh <nileshrege19@gmail.com>
2024-01-06 23:54:03 +0530

    Created shell script for Frontend

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,19 @@
+# Install Nginx
 dnf install nginx -y
+
+# Start & Enable Nginx service
 systemctl enable nginx
 systemctl start nginx
+
+# Remove the default content of nginx web server
 rm -rf /usr/share/nginx/html/*
+
+# Download the frontend content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+
+# Extract the frontend content.
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+# Restart Nginx Service to load the changes of the configuration.
 systemctl restart nginx
